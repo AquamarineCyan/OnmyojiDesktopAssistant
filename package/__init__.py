6223a1a1d5426aeddb919d50ac48ad37dd0521be
AquamarineCyan <1057424730@qq.com>
2022-06-13 15:29:55 +0800

    Update __init__.py

--- package/__init__.py
+++ package/__init__.py
@@ -1,3 +1,21 @@
+from package import (
+    function,
+    update,
+    window,
+    log,
+    yuhun,  # 1
+    yongshengzhihai,  # 2
+    yeyuanhuo,  # 3
+    yuling,  # 4
+    jiejietupo,  # 5&6
+    daoguantupo,  # 7
+    zhaohuan,  # 8
+    baiguiyexing,  # 9
+    huodong  # 10
+)
+from .log import Log
+# from .function import Function
+'''
 from package import function
 from package import update
 from package import window
@@ -10,3 +28,5 @@ from package import jiejietupo  # 5&6
 from package import daoguantupo  # 7
 from package import zhaohuan  # 8
 from package import baiguiyexing  # 9
+from package import huodong # 10
+'''
