2d614f0a3ee424e36cd084999a276f284be156c7
AquamarineCyan <1057424730@qq.com>
2023-04-02 21:37:38 +0800

    :sparkles: 新增更新自动重启

--- utils/upgrade.py
+++ utils/upgrade.py
@@ -6,8 +6,13 @@
 import httpx
 import json
 
+from subprocess import Popen
+from pathlib import Path
+from zipfile import ZipFile
+
 from .config import config
 from .log import log
+from .mysignal import global_ms as ms
 from .toast import toast
 
 
@@ -21,8 +26,9 @@ class Upgrade:
         self.version_github: str = ""
         self.browser_download_url: str = ""
         self.new_version_info: str = ""
+        self.zip_path: str = ""
 
-    def get_browser_download_url(self) -> str:
+    def _get_browser_download_url(self) -> str:
         """获取更新地址
 
         Returns:
@@ -53,29 +59,29 @@ class Upgrade:
         except:
             return "CONNECT ERROR"
 
-    def get_ghproxy_url(self) -> str:
+    def _get_ghproxy_url(self) -> str:
         return f"https://ghproxy.com/{self.browser_download_url}"
 
-    def download_zip(self) -> bool:
+    def _download_zip(self) -> bool:
         """下载更新包"""
         log.ui("准备下载更新包")
         log.ui(f"browser_download_url:{self.browser_download_url}")
-        zip_name = self.browser_download_url.split('/')[-1]
-        log.info(f"zip_name:{zip_name}")
+        self.zip_path = self.browser_download_url.split('/')[-1]
+        log.info(f"zip_name:{self.zip_path}")
         if self.application_path.joinpath(self.browser_download_url.split('/')[-1]) in self.application_path.iterdir():
             log.ui("存在新版本更新包")
             toast("存在新版本更新包", "请关闭程序后手动解压覆盖")
         else:
             log.ui("未存在新版本更新包，即将开始下载")
             try:
-                for download_url in [self.get_ghproxy_url(), self.browser_download_url]:
+                for download_url in [self._get_ghproxy_url(), self.browser_download_url]:
                     log.info(download_url)
                     with httpx.stream("GET", download_url, headers=self.headers) as r:
                         log.info(f"status_code:{r.status_code}")
                         if r.status_code == 200:
                             bytes_total = r.headers["Content-length"]
                             log.ui(f"bytes_total:{bytes_total}")
-                            with open(zip_name, "wb") as f:
+                            with open(self.zip_path, "wb") as f:
                                 for chunk in r.iter_bytes(chunk_size=1024):
                                     if chunk:
                                         f.write(chunk)
@@ -86,17 +92,95 @@ class Upgrade:
                 log.ui("访问下载链接失败")
                 return False
 
-    def upgrade_auto(self):
-        STATUS = self.get_browser_download_url()
+    def check_latest(self) -> None:
+        """检查更新"""
+        STATUS = self._get_browser_download_url()
         match STATUS:
             case "NEW VERSION":
                 log.ui(f"新版本{self.version_github}")
                 toast("检测到新版本", f"{self.version_github}\n{self.new_version_info}")
                 log.ui(self.new_version_info)
-                self.download_zip()
+                self._download_zip()
             case "LATEST":
                 log.ui("暂无更新")
             case "CONNECT ERROR":
                 log.ui("访问更新地址失败")
             case _:
                 log.error("UPDATE ERROR", True)
+        if Path(self.zip_path).exists():
+            ms.qmessagbox_update.emit("question", "更新重启")
+
+    def _unzip_func(self) -> None:
+        log.info("start unzip")
+        # 压缩文件路径
+        # zip_path = Path.cwd() / "Onmyoji_Python-1.7.2.zip"
+        # 文件存储路径
+        self.zip_files_path: Path = self.application_path / "zip_files"
+        # 读取压缩文件
+        for item_path in self.application_path.iterdir():
+            if "Onmyoji_Python" in item_path.name.__str__() and item_path.suffix == ".zip":
+                self.zip_path = item_path
+                break
+        file = ZipFile(self.zip_path)
+        # 解压文件
+        log.ui('开始解压...')
+        file.extractall(self.zip_files_path)
+        log.ui('解压结束')
+
+    def _move_files_recursive(self, source_folder: Path, target_folder: Path) -> None:
+        """递归移动文件"""
+        for item_path in source_folder.iterdir():
+            target_path = target_folder / item_path.name
+            if item_path.is_file() and item_path.name != config.exe_name:  # 排除exe
+                item_path.replace(target_path)
+                self.move_n += 1
+            elif item_path.is_dir():
+                target_path.mkdir(exist_ok=True)
+                self._move_files_recursive(item_path, target_path)
+
+    def _write_restart_bat(self) -> None:
+        """编写bat脚本"""
+        bat_text = f"""@echo off
+@echo 当前为更新程序，等待自动完成
+set "program_name={config.exe_name}"
+
+:a
+tasklist | findstr /I /C:"%program_name%" > nul
+if errorlevel 1 (
+    echo %program_name% is closed.
+    goto :b
+) else (
+    echo %program_name% is still running, waiting...
+    ping 123.45.67.89 -n 1 -w 1000 > nul
+    goto :a
+)
+
+:b
+echo Continue updating...
+
+if not exist zip_files\{config.exe_name} exit
+timeout /T 3 /NOBREAK
+move /y zip_files\{config.exe_name} .
+rd /s /q zip_files
+del {self.zip_path}
+start {config.exe_name}
+"""
+
+        with open("reload.bat", "w", encoding="ANSI") as f:
+            f.write(bat_text)
+
+    def reload(self) -> None:
+        """解压更新包并重启应用程序"""
+        self._unzip_func()
+        self.move_n: int = 0
+        self._move_files_recursive(self.zip_files_path, self.application_path)
+        log.info(f"finish moving {self.move_n} files.")
+        self._write_restart_bat()
+        # 启动.bat文件
+        Popen(['reload.bat'])
+        # 关闭当前exe程序
+        log.info("App Exiting...")
+        ms.sys_exit_update.emit(True)
+
+
+upgrade = Upgrade()
