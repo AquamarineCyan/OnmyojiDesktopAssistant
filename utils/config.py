9f677f53c3d3b7bffa5fbeb541ab4767d5430bd5
AquamarineCyan <1057424730@qq.com>
2023-04-26 19:57:59 +0800

    :package: 1.7.3

--- utils/config.py
+++ utils/config.py
@@ -16,7 +16,7 @@ class Config():
     def __init__(self):
         self.application_name: str = "Onmyoji_Python"
         self.exe_name: str = f"{self.application_name}.exe"
-        self.version: str = "1.7.2"
+        self.version: str = "1.7.3"
         self.application_path: Path = Path.cwd()
         if Path(self.application_path / "resource").is_dir():
             self.resource_path: Path = self.application_path / "resource"
