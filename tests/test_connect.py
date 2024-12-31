74d2fd737993a46d9d14296461b6bddeee5e21a1
AquamarineCyan <1057424730@qq.com>
2024-12-31 19:08:40 +0800

    :construction_worker:

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -9,12 +9,12 @@ headers = {
 def test_api_url():
     api_url = "https://api.github.com/repos/AquamarineCyan/Onmyoji_Python/releases/latest"
     r = httpx.get(api_url, headers=headers)
-    print(f"API URL 请求延迟: {r.elapsed.total_seconds() * 1000} 毫秒")
+    print(f"API URL request latency: {r.elapsed.total_seconds() * 1000} ms")
     assert r.status_code == 200
 
 
 def test_mirror_url():
     mirror_station = "https://ghgo.xyz/"
     r = httpx.get(mirror_station, headers=headers)
-    print(f"镜像站点请求延迟: {r.elapsed.total_seconds() * 1000} 毫秒")
+    print(f"mirror station request latency: {r.elapsed.total_seconds() * 1000} ms")
     assert r.status_code == 200
