bffbbcca21a45ef7c1ff2ecbeb6904b163236879
AquamarineCyan <1057424730@qq.com>
2025-01-19 19:37:01 +0800

    :sparkles: `斗技自动上阵`支持`段位上升`和`周奖励`界面

--- src/package/douji.py
+++ src/package/douji.py
@@ -3,7 +3,8 @@ from ..utils.event import event_thread
 from ..utils.exception import GUIStopException
 from ..utils.function import sleep
 from ..utils.log import logger
-from ..utils.paddleocr import OcrData, check_raw_result_once, ocr, ocr_match_once
+from ..utils.paddleocr import RuleOcr, ocr_match_once
+from ..utils.point import RelativePoint
 from .utils import Package
 
 
@@ -22,7 +23,6 @@ class DouJi(Package):
         self.OCR_FIGHT = self.get_ocr_asset("fight")
         self.OCR_UPDATE_TEAM = self.get_ocr_asset("update_team")
         self.OCR_INTENTIONAL = self.get_ocr_asset("intentional")
-        self.OCR_CONTINUE = self.get_ocr_asset("continue")
         self.OCR_VICTORY = self.get_ocr_asset("victory")
         self.OCR_FAIL = self.get_ocr_asset("fail")
         self.OCR_LEVEL_UP = self.get_ocr_asset("level_up")
@@ -34,7 +34,6 @@ class DouJi(Package):
             self.OCR_FIGHT,
             self.OCR_UPDATE_TEAM,
             self.OCR_INTENTIONAL,
-            self.OCR_CONTINUE,
             self.OCR_VICTORY,
             self.OCR_FAIL,
         ]
@@ -50,40 +49,48 @@ class DouJi(Package):
 
             logger.info(f"current result name: {result.name}")
             match result.name:
-                case "title":
+                case self.OCR_TITLE.name:
                     logger.scene(self.scene_name)
                     msg_title = False
                     # self.check_click(self.OCR_FIGHT, timeout=5)
                     self.current_asset_list.remove(self.OCR_TITLE)
-                case "fight":
+
+                case self.OCR_FIGHT.name:
                     logger.ui("开始战斗")
                     if _flag:
                         continue
                     Mouse.click(result.match_result.center)
                     _flag = True
                     self.current_asset_list.remove(self.OCR_FIGHT)
-                case "update_team":
+
+                case self.OCR_UPDATE_TEAM.name:
                     logger.ui("自动上阵")
                     Mouse.click(result.match_result.center)
-                case "intentional":
+                    sleep(4)
+
+                case self.OCR_INTENTIONAL.name:
                     logger.ui("手动技能")
                     Mouse.click(result.match_result.center)
 
-                case "continue":
+                # 万一对面直接退了呢
+                case self.global_assets.OCR_CLICK_AND_CONTINUE.name:
                     logger.ui("点击屏幕继续")
                     Mouse.click(result.match_result.center)
                     self.done()
                     return
-                case "victory":
+
+                case self.OCR_VICTORY.name:
                     logger.ui("胜利")
                     Mouse.click(result.match_result.center)
                     self.done()
                     return
-                case "fail":
+
+                case self.OCR_FAIL.name:
                     logger.ui_warn("失败")
                     Mouse.click(result.match_result.center)
                     self.done()
                     return
+
                 case _:
                     if msg_title:
                         self.title_error_msg()
@@ -94,12 +101,14 @@ class DouJi(Package):
             if bool(event_thread):
                 raise GUIStopException
 
-            self.fight_once_new()
-            sleep(2, 4)
-            _ocr_data = check_raw_result_once("段位上升")
-            if _ocr_data:
-                _coor = _ocr_data.rect.get_rela_center()
-                logger.ui(f"段位上升 {_coor.coor}")
-                _coor.y += 200
-                Mouse.click(_coor)
-            self.check_click(self.OCR_CONTINUE, timeout=5)  # TODO need test
+            self.fighting_once()
+            sleep(4)
+
+            _ocr = RuleOcr(self.OCR_LEVEL_UP)
+            if result := _ocr.match():
+                logger.ui("段位上升")
+                point = RelativePoint(result.center.x, result.center.y + 200)
+                Mouse.click(point)
+
+            # 每周3胜/5胜奖励
+            self.check_click(self.global_assets.IMAGE_FINISH, timeout=5)
